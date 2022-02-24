7e707e88d400af19e3973b11a84a62cc3d528e5c
david-s-schutte <david.schutte01@gmail.com>
2022-02-24 14:27:36 +1100

    Added a pickup sound effect when picking up a soldier

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,24 +6,57 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    //Gameplay variables
     public int patientsRescued;
     [SerializeField] private Text helicopterPatients;
     [SerializeField] private Text hospitalPatients;
+    [SerializeField] private Text endText;
+    
+    //External References
+    public GameObject[] soldiers;
+    public PlayerController player;
 
+    //Sets the helicopterPatients text to match the player's patientCapacity
     public void SetHelicopterPatientsText(int patientCount){
         helicopterPatients.text = "Soldiers in Helicopter: " + patientCount;
     }
-
+    //Updates patientsRescued and sets the hospitalPatients text to match
     public void SetHospitalPatientsText(int patientCount){
         patientsRescued += patientCount;
-        Debug.Log("Soldiers in Hospital: " + patientsRescued);
         hospitalPatients.text = "Soldiers in Hospital: " + patientsRescued;
+    }
+
+    void Start()
+    {
+        //Define soldiers, player and disable the end text
+        soldiers = GameObject.FindGameObjectsWithTag("Soldier");
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        endText.enabled = false;
     }
 
     void Update()
     {
+        //Allows the reset function
         if(Input.GetKeyDown("r")){
             SceneManager.LoadScene(0);
+        }
+
+        /*Win condition - if patients rescued is equal to the amount of soldiers in the game:
+            - displays the winner text
+            - disables input on the player*/ 
+        if(patientsRescued == soldiers.Length){
+            endText.enabled = true;
+            endText.text = "You Win!";
+            player.playerIsDead = true;
+        }
+        /*Lose condition - if the player crashes into a tree:
+            - makes sure that the player hasn't already finished the game
+            - enables the loser text*/
+        else if(player.playerIsDead){
+            if(!endText.enabled){
+                endText.enabled = true;
+                endText.text = "Game Over";
+            }
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,43 +9,39 @@ public class PlayerController : MonoBehaviour
     public int patientCapacity = 0;
     private int patientLimit = 3;
     GameObject[] patients;
-    private bool playerIsDead = false;
+    public bool playerIsDead = false;
 
     //External References
     private Rigidbody2D rb;
     [SerializeField] private GameManager gameManager;
+    public AudioSource pickupSFX;
 
     void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         gameManager = GameObject.FindWithTag("GameController").GetComponent<GameManager>();
-        Debug.Log("GameManager = " + gameManager);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //If the player isn't dead
+        //If the player isn't dead or hasn't won yet
         if(!playerIsDead){
             //Move them left if they press the left key
             if(Input.GetKey("left")){
                 transform.Translate(new Vector2(-1,0) * flySpeed * Time.deltaTime);
-                Debug.Log("left");
             }
             //Move them right if they press the right key
             if(Input.GetKey("right")){
                 transform.Translate(new Vector2(1,0) * flySpeed * Time.deltaTime);
-                Debug.Log("right");
             }
             //Move them down if they press the down key
             if(Input.GetKey("down")){
                 transform.Translate(new Vector2(0,-1) * flySpeed * Time.deltaTime);
-                Debug.Log("down");
             }
             //Move them up if they press the up key
             if(Input.GetKey("up")){
                 transform.Translate(new Vector2(0,1) * flySpeed * Time.deltaTime);
-                Debug.Log("up");
             }
         }  
     }
@@ -54,12 +50,12 @@ public class PlayerController : MonoBehaviour
         //If the player collides with a tree they die
         if(other.gameObject.tag == "Tree"){
             playerIsDead = true;
-            Debug.Log("u r die");
         }
         //If the player collides with a soldier they need to be picked up
         else if(other.gameObject.tag == "Soldier" && patientCapacity < patientLimit){
             patientCapacity++;
             Destroy(other.gameObject);
+            pickupSFX.Play();
             gameManager.SetHelicopterPatientsText(patientCapacity);
         }
         //IF the player collides with a hospital reset the patient capacity
